9f811ede30329054d1820b0e1178624aa9967e75
Scott Stroz <scott.stroz@oracle.com>
2023-11-27 08:08:59 -0500

    Initial commit of code for app

---
+++ app/DocumentStore.js
@@ -0,0 +1,60 @@
+import * as mysqlx from '@mysql/xdevapi'
+
+class DocumentStore {
+    #schemaName
+    #collectionName
+    #connectionUrl
+    #pool
+    constructor(dbUser, dbPassword, dbHost, dbPort, schemaName, collectionName) {
+        this.#schemaName = schemaName
+        this.#collectionName = collectionName
+        this.#connectionUrl =
+            `mysqlx://${dbUser}:${dbPassword}@${dbHost}:${dbPort}}/${schemaName}`
+        this.#pool = mysqlx.getClient(this.#connectionUrl, {
+            pooling: {
+                enabled: true,
+                maxSize: 10,
+                maxIdleTime: 20000,
+                queueTimeout: 5000
+            }
+        })
+    }
+
+    async getMonths(){
+        const session = await this.#pool.getSession()
+        const schema = session.getSchema(this.#schemaName)
+        const collection = schema.getCollection(this.#collectionName)
+        const result = await collection.find().
+            fields([
+                "date_format(time, '%M %Y') as `full`",
+                "year(time) as `year`",
+                "month(time) as `month`"])
+            .groupBy([
+                "date_format(time, '%M %Y')",
+                "year(time)",
+                "month(time)"])
+            .sort([
+                "year(time) desc",
+                "month(time) desc"])
+            .execute()
+        const data = result.fetchAll()
+        await session.close()
+        return data
+    }
+
+    async getDates(year, month){
+        const session = await this.#pool.getSession()
+        const schema = session.getSchema(this.#schemaName)
+        const collection = schema.getCollection(this.#collectionName)
+        const result = await collection.find("year(time) = :year and month(time) = :month")
+            .bind("year", year)
+            .bind("month", month)
+            .fields(["json_arrayagg(date_format(time, '%d')) as days"])
+            .groupBy("date_format(time, '%d')")
+            .execute()
+        const data = result.fetchAll()
+        await session.close()
+        return data
+    }
+}
+export default DocumentStore
---
+++ app/index.js
@@ -0,0 +1,39 @@
+// importing the dependencies
+import express from 'express'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+// Document Store Service
+import DocumentStore from "./DocumentStore.js";
+const docStore = new DocumentStore(
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    process.env.DB_HOST,
+    process.env.DB_PORT,
+    process.env.DB_SCHEMA,
+    process.env.DB_COLLECTION
+);
+
+
+// defining the Express app
+const app = express()
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(express.json())
+
+// starting the server
+app.listen(process.env.PORT, () => {
+    console.log('listening on port ' + process.env.PORT)
+});
+
+app.get('/months', async (req, res) => {
+    const months = await docStore.getMonths()
+    let msg = {months: months}
+    res.send(msg)
+})
+
+app.get('/dates/:year/:month', async (req, res) => {
+    const dates = await docStore.getDates(req.params.year, req.params.month)
+    let msg = {dates: dates}
+    res.send(msg)
+})
