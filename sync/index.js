9175784b2d9062fb238c6fc6f3673093d7264f3f
Scott Stroz <scott.stroz@oracle.com>
2025-03-03 15:08:22 -0500

    Updated DocStore for sync

--- sync/index.js
+++ sync/index.js
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-import DocumentStore from "DocumentStore.js";
+import DocumentStore from "./DocumentStore.js";
 
 const docStore = new DocumentStore(
     process.env.DB_USER,
